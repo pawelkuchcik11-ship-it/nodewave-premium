6291de0794cd363d21451edf35c47f0219faae58
Pawel <twoj-email@gmail.com>
2026-05-20 18:34:39 +0200

    Add Microsoft Clarity analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,6 +101,18 @@ function RootShell({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script
+  type="text/javascript"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "wu5as0paia");
+    `,
+  }}
+/>
         <HeadContent />
       </head>
       <body>
